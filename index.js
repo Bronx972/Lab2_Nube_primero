35f9b460721e97664aa7eb6f146f7d11854d22ec
Bronx972 <ricardo.guerrero@tecsup.edu.pe>
2023-03-31 18:57:59 -0600

    Agregado

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const express= require('express')
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    
+    res.sendFile('./static/index.html',{
+        root:__dirname
+    })
+    
+})
+
+app.listen(3000)
+console.log('Server onm port 3000')
